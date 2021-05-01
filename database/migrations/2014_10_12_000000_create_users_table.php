7ecb9e1b4696c3f5397e2df2023439b0f30f0dec
한기갑 <kikap.han@musinsa.com>
2021-05-02 07:21:50 +0900

    API 에서 쓸 User 모델 수정

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 20);
+            $table->string('nickname', 30);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone', 20);
             $table->rememberToken();
             $table->timestamps();
         });
